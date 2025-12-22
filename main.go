e4660cc0e7c1df777821ef7deba223313047c757
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-12-21 18:57:45 -0800

    Server now serves index.html for root path.

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	svr := http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
 
+	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
 	log.Fatal(svr.ListenAndServe())
 }
