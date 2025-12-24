d3866794a9cc330dddd1541b8fade7bbc802dd30
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-12-24 12:45:52 -0800

    Add /metrics endpoint and handler.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -21,13 +22,16 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{
+		// fileserverHits: (*atomic.Int32).Load(0),
+	}
 
 	fs := http.FileServer(http.Dir(filepathRoot))
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
 
 	svr := http.Server{
 		Handler: mux,
@@ -48,5 +52,19 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte(msg))
 	if err != nil {
 		log.Println("error writing /healthz response:", err)
+	}
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	_ = r
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	msg := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+
+	_, err := w.Write([]byte(msg))
+	if err != nil {
+		log.Println("error writing /metrics response:", err)
 	}
 }
