2c499c0afbcb297f731208c81b4c050254f22975
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-12-24 12:02:09 -0800

    Add 'apiConfig' struct and associated metrics middleware.

--- main.go
+++ main.go
@@ -3,17 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	mux := http.NewServeMux()
+	apiCfg := apiConfig{}
 
 	fs := http.FileServer(http.Dir(filepathRoot))
-	mux.Handle("/app/", http.StripPrefix("/app", fs))
 
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
 	mux.HandleFunc("/healthz", handlerReadiness)
 
 	svr := http.Server{
