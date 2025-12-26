9cbde316c65f0466e7d52475251b790a13b9e688
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-12-26 11:25:03 -0800

    Updated write contents for metrics and reset handlers.

--- metrics.go
+++ metrics.go
@@ -9,12 +9,19 @@ import (
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	_ = r
 
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(200)
 
-	msg := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	html := fmt.Sprintf(`
+<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+</html>
+		`, cfg.fileserverHits.Load())
 
-	_, err := w.Write([]byte(msg))
+	_, err := w.Write([]byte(html))
 	if err != nil {
 		log.Println("error writing /metrics response:", err)
 	}
@@ -28,7 +35,7 @@ func (cfg *apiConfig) handlerMetricsReset(w http.ResponseWriter, r *http.Request
 
 	cfg.fileserverHits.Store(0)
 
-	msg := fmt.Sprintf("Hits: %v (count reset)", cfg.fileserverHits.Load())
+	msg := fmt.Sprintf("Hits: %v (hits reset)", cfg.fileserverHits.Load())
 
 	_, err := w.Write([]byte(msg))
 	if err != nil {
