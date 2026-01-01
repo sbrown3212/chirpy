45e00150da41e459e3b6a78fb95e6613325ece66
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-12-31 21:11:15 -0700

    Reset now requires 'dev' platform. Reset now deletes all users from db.

--- reset.go
+++ reset.go
@@ -9,18 +9,25 @@ import (
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	_ = r
 
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
 	cfg.fileserverHits.Store(0)
 
+	err := cfg.db.DeleteUsers(r.Context())
+	if err != nil {
+		log.Printf("error deleting users from database: %s", err)
+	}
+
 	msg := fmt.Sprintln("Reset successful")
 
-	_, err := w.Write([]byte(msg))
+	_, err = w.Write([]byte(msg))
 	if err != nil {
 		log.Println("error writing /reset response")
 	}
-
-	// TODO: Make query to delete all users in db
-	// TODO: Delete all users in db within this funciton
 }
