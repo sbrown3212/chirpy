c52e82d7845058350ac6686173aca8dae54c9b83
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-01-05 13:35:43 -0700

    Move chirps handler functions to separate files.

--- handle_chirps_create.go
+++ handle_chirps_create.go
@@ -89,24 +89,3 @@ func cleanseChirpBody(body string, badWords map[string]struct{}) string {
 	cleaned := strings.Join(words, " ")
 	return cleaned
 }
-
-func (cfg *apiConfig) handleGetAllChirps(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.db.GetAllChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "failed to get chirps from db", err)
-		return
-	}
-
-	var chirps []Chirp
-	for _, dbChirp := range dbChirps {
-		chirps = append(chirps, Chirp{
-			ID:        dbChirp.ID,
-			CreatedAt: dbChirp.CreatedAt,
-			UpdatedAt: dbChirp.UpdatedAt,
-			Body:      dbChirp.Body,
-			UserID:    dbChirp.UserID,
-		})
-	}
-
-	respondWithJSON(w, http.StatusOK, chirps)
-}
---
+++ handle_chirps_get.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (cfg *apiConfig) handleGetAllChirps(w http.ResponseWriter, r *http.Request) {
+	dbChirps, err := cfg.db.GetAllChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "failed to get chirps from db", err)
+		return
+	}
+
+	var chirps []Chirp
+	for _, dbChirp := range dbChirps {
+		chirps = append(chirps, Chirp{
+			ID:        dbChirp.ID,
+			CreatedAt: dbChirp.CreatedAt,
+			UpdatedAt: dbChirp.UpdatedAt,
+			Body:      dbChirp.Body,
+			UserID:    dbChirp.UserID,
+		})
+	}
+
+	respondWithJSON(w, http.StatusOK, chirps)
+}
