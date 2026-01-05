cf6406a5ab411b8176d16666ce231d1d02256a2d
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-01-05 16:00:14 -0700

    Update handleCreateUser to hash password.

--- handle_users.go
+++ handle_users.go
@@ -3,11 +3,15 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/sbrown3212/chirpy/internal/auth"
+	"github.com/sbrown3212/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) handleCreateUser(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Email string `json:"email"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
 	type response struct {
 		User
@@ -22,9 +26,18 @@ func (cfg *apiConfig) handleCreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	email := params.Email
+	password := params.Password
+	hash, err := auth.HashedPassword(password)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "failed to hash password", err)
+		return
+	}
 
 	db := cfg.db
-	dbUser, err := db.CreateUser(r.Context(), email)
+	dbUser, err := db.CreateUser(r.Context(), database.CreateUserParams{
+		Email:          email,
+		HashedPassword: hash,
+	})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "unable to create user in database", err)
 	}
