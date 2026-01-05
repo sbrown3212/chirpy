bc4ab3379c178edbb2d774630220120cbe673cbe
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-01-05 11:31:45 -0700

    Fix missing NOT NULL for user_id foreign key for chirps table migration.

--- sql/schema/002_chirps.sql
+++ sql/schema/002_chirps.sql
@@ -4,7 +4,7 @@ CREATE TABLE chirps (
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
   body TEXT NOT NULL,
-  user_id UUID REFERENCES users(id) ON DELETE CASCADE
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- +goose Down
