3377888ca7cc06c091a2000415360ca8fe987d6a
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-01-07 21:10:52 -0700

    Create initial MakeJWT and ValidateJWT funcs.

---
+++ internal/auth/jwt.go
@@ -0,0 +1,43 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	claims := &jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString([]byte(tokenSecret))
+	return ss, err
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := &jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	userIDString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("failed to get subject claim: %w", err)
+	}
+	userID, err := uuid.FromBytes([]byte(userIDString))
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("failed to create uuid: %w", err)
+	}
+
+	return userID, nil
+}
