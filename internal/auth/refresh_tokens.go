482015a90e3fe3025bc276c7a207ff19f07f5ee1
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-01-12 16:02:52 -0700

    Rename refresh_tokens.go for clarity.

---
+++ internal/auth/refresh_tokens.go
@@ -0,0 +1,17 @@
+package auth
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func MakeRefreshToken() (string, error) {
+	token := make([]byte, 32)
+	_, err := rand.Read(token)
+	if err != nil {
+		return "", err
+	}
+
+	tokenStr := hex.EncodeToString(token)
+	return tokenStr, nil
+}
