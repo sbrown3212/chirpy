1028fc2d564b0247bb5a43592833e3adedbaeb51
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-01-13 21:24:14 -0700

    Create UpgradeUserToRed query.

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -96,3 +96,23 @@ func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, e
 	)
 	return i, err
 }
+
+const upgradeUserToRed = `-- name: UpgradeUserToRed :one
+Update users SET is_chirpy_red = true
+WHERE id = $1
+RETURNING id, created_at, updated_at, email, hashed_password, is_chirpy_red
+`
+
+func (q *Queries) UpgradeUserToRed(ctx context.Context, id uuid.UUID) (User, error) {
+	row := q.db.QueryRowContext(ctx, upgradeUserToRed, id)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Email,
+		&i.HashedPassword,
+		&i.IsChirpyRed,
+	)
+	return i, err
+}
