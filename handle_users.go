aa7ed5dc4f258bd3913da8977f40e0fb41eda729
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-01-14 16:57:18 -0700

    Update handleCreateUser to prevent empty email and/or password.

--- handle_users.go
+++ handle_users.go
@@ -26,7 +26,15 @@ func (cfg *apiConfig) handleCreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	email := params.Email
+	if email == "" {
+		respondWithError(w, http.StatusBadRequest, "email must not be empty", nil)
+		return
+	}
 	password := params.Password
+	if password == "" {
+		respondWithError(w, http.StatusBadRequest, "password must not be empty", nil)
+		return
+	}
 	hash, err := auth.HashPassword(password)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "failed to hash password", err)
