003c2f612f620624e5971b86883b405b2382287b
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-01-16 11:57:16 -0700

    Fix Chirp struct typo.

--- handle_chirps_create.go
+++ handle_chirps_create.go
@@ -15,7 +15,7 @@ import (
 type Chirp struct {
 	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"udpated_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 	Body      string    `json:"body"`
 	UserID    uuid.UUID `json:"user_id"`
 }
